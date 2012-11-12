0be6c06822ad7d136255c360a817b22c7e4001ea
Chris Woodward <admin@inb4u.com>
2012-11-12 13:19:10 -0800

    Added .stopchat to manually stop chat with bot

--- source/IRCBot/Modules/chat.cs
+++ source/IRCBot/Modules/chat.cs
@@ -40,6 +40,83 @@ namespace IRCBot.Modules
 
         public override void control(bot ircbot, ref IRCConfig conf, int module_id, string[] line, string command, int nick_access, string nick, string channel, bool bot_command, string type)
         {
+            string module_name = ircbot.conf.module_config[module_id][0];
+            if (type.Equals("channel") && bot_command == true)
+            {
+                foreach (List<string> tmp_command in conf.command_list)
+                {
+                    if (module_name.Equals(tmp_command[0]))
+                    {
+                        string[] triggers = tmp_command[3].Split('|');
+                        int command_access = Convert.ToInt32(tmp_command[5]);
+                        string[] blacklist = tmp_command[6].Split(',');
+                        bool blocked = false;
+                        bool cmd_found = false;
+                        bool spam_check = Convert.ToBoolean(tmp_command[8]);
+                        foreach (string bl_chan in blacklist)
+                        {
+                            if (bl_chan.Equals(channel))
+                            {
+                                blocked = true;
+                                break;
+                            }
+                        }
+                        if (spam_check == true)
+                        {
+                            if (ircbot.spam_activated == true)
+                            {
+                                blocked = true;
+                            }
+                        }
+                        foreach (string trigger in triggers)
+                        {
+                            if (trigger.Equals(command))
+                            {
+                                cmd_found = true;
+                                break;
+                            }
+                        }
+                        if (blocked == false && cmd_found == true)
+                        {
+                            foreach (string trigger in triggers)
+                            {
+                                switch (trigger)
+                                {
+                                    case "stopchat":
+                                        if (spam_check == true)
+                                        {
+                                            ircbot.spam_count++;
+                                        }
+                                        if (nick_access >= command_access)
+                                        {
+                                            foreach (string chat_nick in chatting_nick)
+                                            {
+                                                if (chat_nick.Equals(nick))
+                                                {
+                                                    still_chatting = false;
+                                                    chatting_nick.Clear();
+                                                    chat_time.Enabled = false;
+                                                    chat_time.Stop();
+                                                    ircbot.sendData("PRIVMSG", channel + " :Ok, I will stop.");
+                                                    break;
+                                                }
+                                                else
+                                                {
+                                                    ircbot.sendData("PRIVMSG", channel + " :You are not currently talking to me.");
+                                                }
+                                            }
+                                        }
+                                        else
+                                        {
+                                            ircbot.sendData("NOTICE", nick + " :You do not have permission to use that command.");
+                                        }
+                                        break;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
             if (type.Equals("channel") && bot_command == false)
             {
                 chat_time.Interval = Convert.ToInt32(conf.module_config[module_id][3]) * 1000;
